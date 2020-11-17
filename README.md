09b750adb0f93ae307e3690d277e70b052b3674f
Alikalantaripor <ali.kalantari5731@gmail.com>
2020-11-17 15:00:59 +0330

    Update Code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # List-of-user-agents
-List of major web + mobile browser user agent strings. +1 Bonus script to scrape :) 
+List of major web + mobile browser user agent strings.
+
+#Usage
+Download a list today free of charge
+
+OR
+
+Generate the lists yourself (Requires python 3.7)
+
+- ```[sudo] pip install -r requirements.txt```
+- ```python user-agent.py```
--- user-agent.py
+++ user-agent.py
@@ -1,42 +1,44 @@
+import time
+
 import requests as req
 from bs4 import BeautifulSoup
-import time
 
 url = 'http://www.useragentstring.com/pages/useragentstring.php?name=Firefox'
 
-def save(br,ua):
-	
-	file = br+'.txt'
 
-	with open(file,'ab') as f:
-		f.write(ua+'\n')
+def save(br, list):
+    file = br + '.txt'
+
+    with open(file, 'w') as f:
+        for item in list:
+            f.write("%s\n" % item)
+
 
 def getUa(br):
+    url = 'http://www.useragentstring.com/pages/useragentstring.php?name=' + br
+    r = req.get(url)
 
-	url = 'http://www.useragentstring.com/pages/useragentstring.php?name='+br
-	r = req.get(url)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+    else:
+        soup = False
 
-	if r.status_code == 200:
-		soup = BeautifulSoup(r.content,'html.parser')
-	else:
-		soup = False
+    if soup:
+        div = soup.find('div', {'id': 'liste'})
+        lnk = div.findAll('a')
 
-	if soup:
-		div = soup.find('div',{'id':'liste'})
-		lnk = div.findAll('a')
-
-		for i in lnk:
-			try:
-				save(br,i.text)
-			except:
-				print 'no ua'
-	else:
-		print 'No soup for '+br
+        for_save = []
+        for i in lnk:
+            for_save.append(i.text)
+            # save(br, i.text)
+        print(for_save)
+        save(br, for_save)
+    else:
+        print('No soup for ' + br)
 
 
-
-lst = ['Firefox','Internet+Explorer','Opera','Safari','Chrome','Edge','Android+Webkit+Browser']
+lst = ['Firefox', 'Opera', 'Safari', 'Chrome', 'Edge', 'Android+Webkit+Browser']
 
 for i in lst:
-	getUa(i)
-	time.sleep(20)
+    getUa(i)
+    time.sleep(5)
